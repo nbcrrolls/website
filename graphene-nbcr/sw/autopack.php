a560a7473c1db57759403eed75ce359d750d2f70
nadyawilliams <nadya@sdsc.edu>
2015-01-09 18:46:24 -0800

    add sw items lipidwrapper and autopack

---
+++ graphene-nbcr/sw/autopack.php
@@ -0,0 +1,30 @@
+<?php 
+/* Software info */
+$acknowledgment_specific='';
+$sw_version='0.5';
+$sw_name = 'autoPACK';
+$sw_description = '
+autoPack is an open-source general packing algorithm that positions 3D objects on surfaces and into/around volumes with zero to minimal overlap depending on the method used and the accuracy vs speed parameters selected by the user. It provides a general architecture to allow various packing algorithms to interoperate efficiently in the same model. 
+';
+$sw_platforms='Windows, Mac, or Linux';
+$sw_citation_bib_link = '';
+$sw_citation_endnote_link = '';
+$sw_citation_plain_link = '';
+$sw_license='';
+$sw_image = 'autopack.png';
+
+/* Software links options */
+$link_download = 'http://www.autopack.org/install';
+$link_documentation = 'http://www.autopack.org/documentation';
+$link_users_guide = 'http://www.autopack.org/documentation/gui-guide';
+$link_tutorials = 'http://www.autopack.org/documentation/tutorials';
+$link_datasets = '';
+$link_mailing_lists = 'http://www.autopack.org/support/forum';
+$link_bug_report = '';
+
+/* Software web service */
+$link_webservice1 = '';
+$link_webservice2 = '';
+$link_webservice3 = '';
+?>
+
