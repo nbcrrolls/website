9f83a2a0ec6d6fa7596fa096c9814d4b8c5c487a
nadyawilliams <nadya@sdsc.edu>
2017-01-10 11:53:55 -0800

    rm version from css and js files

--- graphene-nbcr/functions.php
+++ graphene-nbcr/functions.php
@@ -345,4 +345,14 @@ function show_tutorials () {
     }
 }
 
+// 2016-12-23 remvoe ver from css and js files
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+
 ?>
